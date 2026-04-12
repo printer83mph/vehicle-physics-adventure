cb8542545673810f887b0ed9b5e6a26611dbc107
Thomas Shaw <printer.83mph@gmail.com>
2026-04-12 15:41:48 -0400

    Use rotation for car to spin around

--- app/entities/vehicle.py
+++ app/entities/vehicle.py
@@ -1,7 +1,7 @@
 from typing import override
 
 import numpy as np
-import pyray
+import pyray as rl
 from pyray import RED
 
 from app.entities.base import BaseEntity, EntityTickBundle
@@ -15,7 +15,13 @@ class Vehicle(BaseEntity):
     @override
     def tick(self, bundle: EntityTickBundle) -> None:
         self.position[0] = np.sin(bundle.elapsed_time * np.pi) * 80
+        self.rotation += bundle.dt * np.pi * 2
+
+        self.rotation %= 360
+
+    body: rl.Rectangle = rl.Rectangle(-20, -40, 40, 80)
 
     @override
     def draw(self) -> None:
-        pyray.draw_rectangle(self.position[0] - 20, self.position[1] - 20, 40, 40, RED)
+        with self.rl_transform_local():
+            rl.draw_rectangle_rec(self.body, RED)
