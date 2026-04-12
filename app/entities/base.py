6e1676340167cdf1cedfe7ebee5fa0d6efc34fbc
Thomas Shaw <printer.83mph@gmail.com>
2026-04-12 15:41:28 -0400

    Add rotation to BaseEntity
    - also context manager for raylib transform

--- app/entities/base.py
+++ app/entities/base.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 
+from contextlib import contextmanager
 from dataclasses import dataclass
+from typing import Generator
 
 import numpy as np
+import pyray as rl
 
 
 @dataclass
@@ -20,9 +23,17 @@ class BaseEntity:
 
     position: np.typing.NDArray[np.float64]
     "`[x, y]` - World position"
+    rotation: float
+    "Rotation, in radians"
 
-    def __init__(self, x: float, y: float):
-        self.position = np.array([x, y])
+    def __init__(
+        self,
+        *,
+        pos: tuple[float, float] = (0, 0),
+        rotation: float = 0,
+    ):
+        self.position = np.array(pos)
+        self.rotation = rotation
 
     def tick(self, bundle: EntityTickBundle) -> None:
         pass
@@ -32,3 +43,15 @@ class BaseEntity:
 
     def draw(self) -> None:
         pass
+
+    @contextmanager
+    def rl_transform_local(self) -> Generator[None]:
+        """Temporarily push local transform matrices to the stack, including translation and rotation."""
+
+        rl.rl_translatef(self.position[0], self.position[1], 0)
+        rl.rl_rotatef(np.rad2deg(self.rotation), 0, 0, 1)
+        try:
+            yield None
+        finally:
+            rl.rl_pop_matrix()
+            rl.rl_pop_matrix()
